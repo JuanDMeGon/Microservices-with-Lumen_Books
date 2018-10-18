3f20036309d951e25bbaf8448872ca100d5cec40
JuanDMeGon <juandmegon@gmail.com>
2018-10-18 17:04:48 -0500

    Creating the Lumen controller for books

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Return the list of books
+     * @return Illuminate\Http\Response
+     */
+    public function index()
+    {
+    }
+
+    /**
+     * Create one new book
+     * @return Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+    }
+
+    /**
+     * Obtains and show one book
+     * @return Illuminate\Http\Response
+     */
+    public function show($book)
+    {
+    }
+
+    /**
+     * Update an existing book
+     * @return Illuminate\Http\Response
+     */
+    public function update(Request $request, $book)
+    {
+    }
+
+    /**
+     * Remove an existing book
+     * @return Illuminate\Http\Response
+     */
+    public function destroy($book)
+    {
+    }
+}
