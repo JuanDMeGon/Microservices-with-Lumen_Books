b4b22c543498cba4acb79236768921238ee8f59d
JuanDMeGon <juandmegon@gmail.com>
2018-10-18 18:34:45 -0500

    Allowing updating an existing book

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -69,6 +69,26 @@ class BookController extends Controller
      */
     public function update(Request $request, $book)
     {
+        $rules = [
+            'title' => 'max:255',
+            'description' => 'max:255',
+            'price' => 'min:1',
+            'author_id' => 'min:1',
+        ];
+
+        $this->validate($request, $rules);
+
+        $book = Book::findOrFail($book);
+
+        $book->fill($request->all());
+
+        if ($book->isClean()) {
+            return $this->errorResponse('At least one value must change', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $book->save();
+
+        return $this->successResponse($book);
     }
 
     /**
