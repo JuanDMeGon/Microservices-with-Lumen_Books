e446c0d9a8f015668eb6dd463dd376ccfb47740f
JuanDMeGon <juandmegon@gmail.com>
2018-10-18 17:15:52 -0500

    Standardizing the responses across the services

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
+    use ApiResponser;
+
     /**
      * Create a new controller instance.
      *
