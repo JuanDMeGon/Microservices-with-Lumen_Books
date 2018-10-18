fb7c58814f0a6bb3c948dbb17bb89abf3d5b7e42
JuanDMeGon <juandmegon@gmail.com>
2018-10-18 17:09:21 -0500

    Creating the Lumen routes for CRUD operations on books

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
-});
+$router->get('/books', 'BookController@index');
+$router->post('/books', 'BookController@store');
+$router->get('/books/{book}', 'BookController@show');
+$router->put('/books/{book}', 'BookController@update');
+$router->patch('/books/{book}', 'BookController@update');
+$router->delete('/books/{book}', 'BookController@destroy');
