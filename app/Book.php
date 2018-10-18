ed0c6dc2b15a700dc595f5a4f16a81dd6d97cb25
JuanDMeGon <juandmegon@gmail.com>
2018-10-18 17:04:27 -0500

    Creating the model for books

---
+++ app/Book.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'price',
+        'author_id',
+    ];
+}
